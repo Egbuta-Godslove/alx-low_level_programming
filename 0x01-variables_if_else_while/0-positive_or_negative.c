9f88c7c84215432da27511281ffab3f9cac84c59
Egbuta-Godslove <godsloveegbuta6@gmail.com>
2022-12-09 13:54:56 -0800

    sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,6 +13,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
+
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
@@ -26,4 +27,5 @@ int main(void)
 		printf("%d is zero\n", n);
 	}
 	return (0);
+
 }
