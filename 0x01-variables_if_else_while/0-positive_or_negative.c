fb5134f9f5225706ac858ddc5560cc3aa7a6a728
Egbuta-Godslove <godsloveegbuta6@gmail.com>
2022-12-09 14:58:22 -0800

    nventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,18 +2,16 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- */
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
 int main(void)
-
 {
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
@@ -27,5 +25,4 @@ int main(void)
 		printf("%d is zero\n", n);
 	}
 	return (0);
-
 }
